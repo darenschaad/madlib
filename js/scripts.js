83b3da19e627999f039a2cfca79ca0fe50629838
Daren Schaad and Neil Larion <neil.larion@gmail.com>
2016-01-13 16:17:06 -0800

    change out the forEach loop with a standard for loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,9 @@ $(document).ready(function() {
 
 
   $("#blanks form").submit(function(event) {
-    madlib.forEach(function(word){
-      $("." + word).text($("input#" + word).val());
-    })
-    // var adjective1Input = $("input#adjective1").val();
-    // var person1Input = $("input#person1").val();
-    // var animal1Input = $("input#animal1").val();
-    // var animal2Input = $("input#animal2").val();
-    // var verbpastInput = $("input#verbpast").val();
-    // var adjective2Input = $("input#adjective2").val();
-    // var adjective3Input = $("input#adjective3").val();
-    //
-    //
-    // $(".adjective1").text(adjective1Input);
-    // $(".person1").text(person1Input);
-    // $(".animal1").text(animal1Input);
-    // $(".animal2").text(animal2Input);
-    // $(".verbpast").text(verbpastInput);
-    // $(".adjective2").text(adjective2Input);
-    // $(".adjective3").text(adjective3Input);
-
+    for(var i=0; i< madlib.length; i++){
+      $("." + madlib[i]).text($("input#" + madlib[i]).val());
+    }
     $("#story").show();
 
     event.preventDefault();
